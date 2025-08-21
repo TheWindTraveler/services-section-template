e9d9c7456f901e19ad7a2d5d4b09666ea0faf3de
Begotten-Traveler <solomondev002@gmail.com>
2025-08-21 19:40:47 +0100

    add: all project

---
+++ index.js
@@ -0,0 +1,23 @@
+ const buttons = document.querySelectorAll('.services-nav-button');
+const services = document.querySelectorAll('.services');
+
+// Loop through each button
+    buttons.forEach(btn => {
+      btn.addEventListener('click', () => {
+        // Remove every active class from all
+        buttons.forEach(button => button.classList.remove('active'));
+        btn.classList.add('active');
+
+        const filter = btn.getAttribute('data-filter');
+        console.log(filter);
+
+        // Loop through each services
+        services.forEach(service => {
+          if (filter === "all" || service.getAttribute('data-category') === filter) {
+            service.style.display = "block";
+          } else {
+            service.style.display = "none";
+          }
+        });
+      });
+    });
